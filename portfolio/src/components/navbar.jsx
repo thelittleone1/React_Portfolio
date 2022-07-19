ec1e6d944ddf5d940c4baa78118ff93f32b5d4f7
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-18 17:01:31 -0700

    working on nav styling functions, added hamburg menu

--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TempLogo from "../assets/github.png"
 import {FaBars, FaTimes} from "react-icons/fa"
 
 const Navbar = () => {
+
+  const[nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
   return (
     
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#31293F] text-gray-100">
@@ -23,17 +27,18 @@ const Navbar = () => {
       </div>
 
       {/*Hamburger Menu*/}
-      <div className='md:hidden'>
-        <FaBars />
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+        
       </div>
 
       {/*Mobile Menu*/}
-      <ul className='absolute top-0 left-0 w-full h-screen bg-[#31293F] flex flex-col justify-center items-center'>
-      <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Portfolio</li>
-          <li>Contact</li>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#31293F] flex flex-col justify-center items-center'}>
+          <li className='py-4 text-3xl'>Home</li>
+          <li className='py-4 text-3xl'>About</li>
+          <li className='py-4 text-3xl'>Skills</li>
+          <li className='py-4 text-3xl'>Portfolio</li>
+          <li className='py-4 text-3xl'>Contact</li>
       </ul>
 
     </div>
