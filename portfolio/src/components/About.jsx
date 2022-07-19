013e01dc43abfdbcb12c5702ef62a2304ecfcbc9
Urooj Qureshi <36391381+thelittleone1@users.noreply.github.com>
2022-07-19 16:55:16 -0700

    working on about section

---
+++ portfolio/src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#D3D9E9] text-[black]'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full x-4 grid gird-col-2 gap-89'>
+                <div className='sm:text-rigth pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-[#A6A9C8]'>About</p>
+                </div>
+                    
+                <div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About;
--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         {/*Container*/}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
             <p className='text-4xl sm:text-7xl '>Hello, my name is Urooj Qureshi</p>
-            <h1 className='text-3xl sm:text-4xl text-[white]'>I'm a full stack developer with a focus on UX design.</h1>
+            <h1 className='text-3xl sm:text-4xl text-[white] py-3'>I'm a full stack developer with a focus on UX design.</h1>
 
             <div>
                 <button className='text-white bg-[#28398d] border-2 px-6 py-2 my-5 flex items-center hover:bg-[#7387c3]'>View Work </button>
